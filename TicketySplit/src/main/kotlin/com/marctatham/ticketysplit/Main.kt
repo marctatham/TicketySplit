34078862eebe9b9997890b01a65a6d15f5d637de
Marc Tatham <marctatham@gmail.com>
2025-04-07 10:10:51 +0200

    fix basic auth configuration

--- TicketySplit/src/main/kotlin/com/marctatham/ticketysplit/Main.kt
+++ TicketySplit/src/main/kotlin/com/marctatham/ticketysplit/Main.kt
@@ -7,9 +7,11 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.client.request.*
+import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
+import java.util.*
 
 
 private val logger = KotlinLogging.logger {}
@@ -18,23 +20,28 @@ fun main(args: Array<String>) = runBlocking {
     logger.info { ("Hello, World! $") }
 
     // let's query a ticket status to validate that it's all setup
-    // todo: fetch from local properties or similar
-    val ticket = getJiraTicketDetails("DJMA-5836", "")
+    val ticket = getJiraTicketDetails("DJMA-5836")
     logger.info { "ticket: $ticket" }
 
 }
 
-suspend fun getJiraTicketDetails(ticketId: String, apiToken: String): JiraTicket {
+suspend fun getJiraTicketDetails(ticketId: String): JiraTicket {
     val client = HttpClient(CIO) {
         install(ContentNegotiation) { json() }
         install(Logging) { level = LogLevel.ALL }
     }
 
+    val email = System.getenv("JIRA_USERNAME") ?: error("JIRA_USERNAME environment variable not set")
+    val apiToken = System.getenv("JIRA_API_TOKEN") ?: error("JIRA_API_TOKEN environment variable not set")
+    val authString = "$email:$apiToken"
+    val encodedAuth = Base64.getEncoder().encodeToString(authString.toByteArray())
+    logger.info { "with token: $encodedAuth" }
+
     val response: JiraTicket = client.use {
         it.get("https://dowjones.atlassian.net/rest/api/3/issue/$ticketId") {
             headers {
-                append("Authorization", "Basic $apiToken")
-                append("Accept", "application/json")
+                append(HttpHeaders.Authorization, "Basic $encodedAuth")
+                append(HttpHeaders.Accept, "application/json")
             }
         }.body()
     }
