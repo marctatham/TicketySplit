215e73395c64bf16f0cf689d581f4be649c946d8
Marc Tatham <marctatham@gmail.com>
2025-04-10 13:56:43 +0200

    make it a bit clearer which function is being executed
    Note: problem with `Fields` structure being different for GetTicket vs CreateTicket :firstworldproblems:

--- TicketySplit/src/main/kotlin/com/marctatham/ticketysplit/Main.kt
+++ TicketySplit/src/main/kotlin/com/marctatham/ticketysplit/Main.kt
@@ -1,5 +1,6 @@
 package com.marctatham.ticketysplit
 
+import com.marctatham.ticketysplit.common.jira.action.getJiraTicketDetails
 import com.marctatham.ticketysplit.domain.bulkCreateTickets
 import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
@@ -9,16 +10,24 @@ private val logger = KotlinLogging.logger {}
 
 fun main(args: Array<String>) = runBlocking {
 
-    // let's query a ticket status to validate that it's all setup
-//    val ticket = getJiraTicketDetails("DJMA-5506")
-    logger.info { "TicketySplit init" }
+    //getTicket()
 
+    bulkLoad()
+}
+
+
+private suspend fun bulkLoad() {
     // todo: parse the CSV file
     // todo: create a ticket for each row
     val filePath = "/Users/marctatham/Downloads/taskbreakdown.csv"
     bulkCreateTickets(filePath)
 
     logger.info { "TicketySplit complete" }
+}
+
+private suspend fun getTicket() {
+    // let's query a ticket status to validate that it's all setup
+    val ticket = getJiraTicketDetails("DJMA-5506")
 }
 
 
